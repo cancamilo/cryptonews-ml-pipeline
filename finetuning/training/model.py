e6c2bad2fd4e1612f91eefb4674abb882e018e83
cancamilo <camiloramirezf@hotmail.com>
2024-06-12 16:43:49 +0200

    fix import

--- finetuning/training/model.py
+++ finetuning/training/model.py
@@ -8,8 +8,8 @@ import torch as th
 import yaml
 from comet_ml import Experiment
 from datasets import DatasetDict, load_dataset
-from finetuning.dataset_client import DatasetClient
-from finetuning.settings import settings
+from training.dataset_client import DatasetClient
+from training.settings import settings
 from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training
 from qwak.model.adapters import DefaultOutputAdapter
 from qwak.model.base import QwakModel

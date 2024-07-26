2d6e76b5e89f072bd99db3582713d2ec0fe8ebf6
cancamilo <camiloramirezf@hotmail.com>
2024-07-26 13:18:01 +0200

    port scrapping to class

--- data_ingestion_pipeline/crawlers/news_btc.py
+++ data_ingestion_pipeline/crawlers/news_btc.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from typing import List, Any
 import logging
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from data_ingestion_pipeline.models.documents import ArticleDocument
+from datetime import datetime, timedelta
 from crawlers.base import BaseCrawler
 from models.documents import ArticleDocument
 from config import settings
@@ -19,6 +22,13 @@ class NewsBTCCrawler(BaseCrawler):
     name = "coin_telegraph_crawler"
     date_format = "%Y-%m-%d"
     url = "https://cointelegraph.com/"
+
+    date_map = {
+        "minute": 1,
+        "hour": 60,
+        "day": 24 * 60,
+        "week": 7 * 24 * 60
+    }
 
     def __init__(self, dev=False) -> None:
         super().__init__()
@@ -49,35 +59,80 @@ class NewsBTCCrawler(BaseCrawler):
                 service=webdriver.ChromeService("/opt/chromedriver"), options=options
             )
 
+    def get_minutes(self, date_string):
+        "Given a string with a number and a time period, return the equivalent number of minutes. e.g 1 hour -> 60 minutes"
+        parts = date_string.split(" ")
+        num = int(parts[0]) 
+        window = parts[1].replace("s", "")
+        minutes = num * self.date_map[window]
+        return minutes
+
+    def extract_article(self, link):
+        "Extract the titles and paragraphs of a page with an article"
+
+        parsed_url = urlparse(link)
+
+        # Split the path to get components
+        path_components = parsed_url.path.split('/')
+
+        # Extract the topic and title
+        topic = path_components[2]  # 'company'
+        title = path_components[-2].replace('-', ' ')
+
+        # Parse the page source
+        self.driver.get(link)
+        time.sleep(1) # wait for content to load     
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+        # Find date
+        delta_text = soup.find("div", class_="jeg_meta_date").find("div").get_text().strip()
+        dt = datetime.now() - timedelta(minutes=self.get_minutes(delta_text))
+        date = datetime.strftime(dt, self.date_format)
+
+        soup.find('div', class_='entry_content')
+
+        # Find all h1, h2 and p elements within the jeg_inner_content div
+        relevant_elements = soup.find('div', class_='content-inner').find_all(['h1', 'h2', 'p'])
+
+        # Extract the text content from each element
+        extracted_text = [element.text.strip() for element in relevant_elements]
+        text = " ".join(extracted_text)
+
+        # Print the extracted text
+        return ArticleDocument(source="newsbtc", title=title, content=text, date=date)
+
     def extract(
         self,
         n_scrolls,
         start_date=datetime.now().strftime(date_format),
         end_date=datetime.now().strftime(date_format),
     ) -> List[Any]:
-        """Scrap news from cointelegraph"""
+        """Scrap news from news btc"""
 
         url = "https://www.newsbtc.com/news/"
-
         self.driver.get(url)
 
-        # Find the list of articles and extract links 
-        links = []
+        # Find the list of articles and extract links
+        parent = self.driver.find_element(By.ID, "load-more-article-stream")
 
-        parent = self.driver.find_element(By.ID)
+        for _ in range(n_scrolls):
+            self.driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+            
+            # load more button
+            load_button = self.driver.find_element(By.ID, "load-more-container")
+            load_button.click()
+            time.sleep(1)
+
+        # After clicking the load button, collect all article elements and extract their links
         article_elements = parent.find_elements(By.TAG_NAME, 'article')
         links = [article.find_element(By.TAG_NAME, 'a').get_attribute('href') for article in article_elements]
 
-        # articles = []
-        # articles.append(
-        #     ArticleDocument(
-        #         source="coin_telepraph",
-        #         title=title,
-        #         summary=summary,
-        #         content=content,
-        #         published_at=datetime,
-        #     )
-        # )
+        # Visit each link saved before
+        articles = []
+        for link in links:
+            text = self.extract_article(link)
+            articles.append(text)
+            time.sleep(2)  # Adjust the sleep time as needed to allow the page to load
 
         self.driver.close()
 
